20caa92a3f1f4305608f494f04ba8a97e1ddcae3
Gabe <sentiinex@gmail.com>
2018-02-10 14:15:25 -0800

    Add GPAlertController and GPAlertView

---
+++ GPAlertController/GPAlertController/GPAlertController.swift
@@ -0,0 +1,52 @@
+//
+//  GPAlertController.swift
+//  GPAlertController
+//
+//  Created by Gabriele Pregadio on 2/10/18.
+//  Copyright © 2018 Gabriele Pregadio. All rights reserved.
+//
+
+import UIKit
+
+typealias GPAlertActionHandler = ((GPAlertAction) -> Void)
+open class GPAlertAction: NSObject {
+    var title: String?
+    var handler: GPAlertActionHandler?
+    init(title: String?, handler: GPAlertActionHandler? = nil) {
+        self.title = title
+        self.handler = handler
+    }
+}
+
+open class SFAlertController: UIViewController {
+    
+    // MARK: Variables
+    private var alertView: GPAlertView?
+    private lazy var firstAction: GPAlertAction = { [unowned self] in
+        // TODO: Use title defined by user in constants
+        return GPAlertAction(title: "") { _ in
+            self.dismiss(animated: true)
+        }
+    }()
+    private var secondAction: GPAlertAction?
+    // TODO: Style
+    var alertTitle: String?
+    var alertDetail: String?
+    private var isFirstActionCustom = false
+    override open var modalPresentationStyle: UIModalPresentationStyle {
+        get { return .overCurrentContext }
+        set {  }
+    }
+    open override var modalTransitionStyle: UIModalTransitionStyle {
+        get { return .crossDissolve }
+        set {  }
+    }
+    
+    // MARK: Init
+    // TODO: Style in init
+    convenience init(title: String?, message: String?) {
+        self.init(nibName: nil, bundle: Bundle(for: type(of: self)))
+        alertTitle = title
+    }
+    
+}
---
+++ GPAlertController/GPAlertController/GPAlertView.swift
@@ -0,0 +1,63 @@
+//
+//  GPAlertView.swift
+//  GPAlertController
+//
+//  Created by Gabriele Pregadio on 2/10/18.
+//  Copyright © 2018 Gabriele Pregadio. All rights reserved.
+//
+
+import UIKit
+
+internal protocol GPAlertViewDelegate: class {
+    func gpAlertViewDidTapFirstButton(_ alertView: GPAlertView)
+    func gpAlertViewDidTapSecondButton(_ alertView: GPAlertView)
+}
+
+internal class GPAlertView: UIView {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var topSeparatorView: UIView!
+    @IBOutlet weak var buttonsSeparatorView: UIView!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    
+    weak var delegate: GPAlertViewDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: 270).isActive = true // TODO: Think of a better way
+    }
+    
+    // TODO: Style
+    func configure(title: String?, message: String?, firstButtonText: String?, secondButtonText: String?) {
+        if title == nil {
+            titleLabel.isHidden = true
+        } else {
+            titleLabel.text = title
+        }
+        if message == nil {
+            detailLabel.isHidden = true
+        } else {
+            detailLabel.text = message
+        }
+        firstButton.setTitle(firstButtonText, for: .normal)
+        if secondButtonText == nil {
+            buttonsSeparatorView.isHidden = true
+            secondButton.isHidden = true
+        } else {
+            secondButton.setTitle(secondButtonText, for: .normal)
+        }
+        sizeToFit()
+    }
+    
+    @IBAction func tapFirstButton(_ sender: Any) {
+        delegate?.gpAlertViewDidTapFirstButton(self)
+    }
+    
+    @IBAction func tapSeconfButton(_ sender: Any) {
+        delegate?.gpAlertViewDidTapSecondButton(self)
+    }
+    
+}
