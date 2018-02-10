59078323bf0a8219aad3cd5f49b7a24b439732be
Gabe <sentiinex@gmail.com>
2018-02-10 15:44:28 -0800

    Display alert view

--- GPAlertController/GPAlertController/GPAlertController.swift
+++ GPAlertController/GPAlertController/GPAlertController.swift
@@ -34,9 +34,80 @@ open class GPAlertController: UIViewController {
     
     // MARK: Init
     // TODO: Style in init
-    convenience init(title: String?, message: String?) {
+    public convenience init(title: String?, message: String?) {
         self.init(nibName: nil, bundle: Bundle(for: type(of: self)))
         alertTitle = title
     }
     
+    // MARK: UIViewController
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        providesPresentationContextTransitionStyle = true
+        definesPresentationContext = true
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        view.isOpaque = false
+    }
+    
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareView()
+    }
+    
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        alertView?.center = view.center
+    }
+    
+    // MARK: Instance methods
+    private func prepareView() {
+        let nib = UINib(nibName: String(describing: GPAlertView.self), bundle: Bundle(for: GPAlertView.self))
+        guard let alertView = nib.instantiate(withOwner: self).first as? GPAlertView else { return } // TODO: Handle error
+        alertView.delegate = self
+        alertView.configure(title: alertTitle, message: alertDetail, firstButtonText: firstAction.title, secondButtonText: secondAction?.title)
+        self.alertView = alertView
+        view.addSubview(alertView)
+        alertView.center = view.center
+    }
+    
+    func addAction(_ action: GPAlertAction) {
+        if !isFirstActionCustom {
+            firstAction = action
+            isFirstActionCustom = true
+        } else {
+            secondAction = action
+        }
+    }
+    
+}
+
+// MARK: GPAlertViewDelegate
+extension GPAlertController: GPAlertViewDelegate {
+    func gpAlertViewDidTapFirstButton(_ alertView: GPAlertView) {
+        if let handler = firstAction.handler {
+            handler(firstAction)
+            dismiss(animated: true)
+        } else {
+            dismiss(animated: true)
+        }
+    }
+    
+    func gpAlertViewDidTapSecondButton(_ alertView: GPAlertView) {
+        guard let secondAction = secondAction else {
+            dismiss(animated: true)
+            return
+        }
+        if let handler = secondAction.handler {
+            handler(secondAction)
+            dismiss(animated: true)
+        } else {
+            dismiss(animated: true)
+        }
+    }
+}
+
+// MARK: UIViewControllerTransitioningDelegate
+extension GPAlertController: UIViewControllerTransitioningDelegate {
+    public func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return GPAlertTransitioningAnimator.default
+    }
 }
